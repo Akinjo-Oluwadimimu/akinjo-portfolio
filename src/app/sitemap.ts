cf28b14efc0e87ca9e4ceb401a08b8c8de665055
Akinjo Abiodun <oluwadimimuakinjo@yahoo.com>
2025-10-02 17:46:55 +0100

    SEO Updates

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next'
 import { getDocs, collection } from 'firebase/firestore'
-import { db } from '@/lib/firebase' // your Firebase config import
+import { db } from '@/lib/firebase' 
+
+export const dynamic = "force-dynamic"; 
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://dimimu.dev'
